494f971b450f59a2d01e1c79d55f40dce3c0009b
Jaehyeok <milkboy2564@naver.com>
2022-06-29 14:46:10 +0900

    2022/06/29

---
+++ 0629/src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './Header';
+import {Link, Routes, Route} from 'react-router-dom';
+import {useEffect, useState} from 'react';
+import { Welcome } from './Welcome';
+
+function Nav(props){
+  return <nav>
+    <ol>
+      {props.data.map(e=><li key={e.id}><Link to={`/read/${e.id}`}>{e.title}</Link></li>)}
+    </ol>
+  </nav>
+}
+
+function Read(){
+  return <article>
+    <h2>Read</h2>
+    Hello, Read
+  </article>
+}
+
+function App() {
+  const [topics, setTopics] = useState([]);
+  async function refresh(){
+    const resp = await fetch('http://localhost:3334/topics');
+    const data = await resp.json();
+    setTopics(data);
+  }
+  useEffect(()=>{
+    refresh();
+  }, []);
+  return (
+    <div>
+      <Header></Header>
+      <Nav data={topics}></Nav>
+      <Routes>
+        <Route path="/" element={<Welcome></Welcome>}></Route>
+        <Route path="/read/1" element={<Read></Read>}></Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
